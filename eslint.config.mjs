abc5f562db8d807407c8dc290442aba79375d022
aoaiku0624 <aoaiku0624@gmail.com>
2026-08-30 15:18:33 +0800

    fix: allow feeding during travel

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,8 @@ const eslintConfig = defineConfig([
     "dist/**",
     "out/**",
     "build/**",
+    "**/._*",
+    "Saves/.tools/**",
     "next-env.d.ts",
   ]),
   eslint.configs.recommended,
